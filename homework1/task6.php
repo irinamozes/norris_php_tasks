d0ac51850d393a8cafd3ef32700ac5fe92a9d509
Irina Mozes <irina5687@gmail.com>
2016-11-01 22:35:04 +0500

    исправление task4 и task6

--- homework1/task6.php
+++ homework1/task6.php
@@ -1,32 +1,34 @@
 <?php
-$car1 = [
-    "name" => "bmw",
+$bmw = [
     "model" => "X5",
     "speed" => 120,
     "doors" => 5,
     "year" => "2015",
 ];
 
-$car2 = [
-    "name" => "toyota",
+$toyota = [
     "model" => "M10",
     "speed" => 110,
     "doors" => 4,
     "year" => "2014",
 ];
 
-$car3 = [
-    "name" => "opel",
+$opel = [
     "model" => "T7",
     "speed" => 130,
     "doors" => 4,
     "year" => "2016",
 ];
 
-$cars = [$car1, $car2, $car3];
+$cars = [
+   "bmw" => $bmw,
+   "toyota" => $toyota,
+   "opel" => $opel
+];
+
 
 foreach ($cars as $i => $value) {
-    echo 'CAR '.$cars[$i]["name"]."\n";
-    echo $cars[$i]["model"].' - '.$cars[$i]["speed"].' - '.$cars[$i]["doors"].' - '.$cars[$i]["year"]."\n";
+    echo "CAR $i\n";
+    echo $value["model"].' - '.$value["speed"].' - '.$value["doors"].' - '.$value["year"]."\n";
 }
 ?>
