911083ced5fbc877be3e6b1901db5e4cab1e6e4b
Irina Mozes <irina5687@gmail.com>
2016-11-25 00:19:23 +0500

    проблемные файлы в hw3_copy

--- hw3_copy/create_tables.php
+++ hw3_copy/create_tables.php
@@ -1,3 +1,13 @@
+| Database           |
++--------------------+
+| information_schema |
+| lschool_db         |
+| mysql              |
+| performance_schema |
++--------------------+
+4 rows in set (0,01 sec)
+
+
 <?php
 
 $host = 'localhost';
@@ -27,8 +37,7 @@ $sql = "
   username varchar(100) NOT NULL,
   age int(3),
   info text,
-  user_id int(5) NOT NULL,
-  PRIMARY KEY (profile_id),
+  PRIMARY KEY (id),
   FOREIGN KEY (user_id) REFERENCES users_login (user_id)
   ON UPDATE CASCADE
   ON DELETE CASCADE
@@ -39,4 +48,16 @@ $sql = "
   CREATE TABLE user_save (
   user_id int(5) NOT NULL,
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;";
+$connection->query($sql);
+
+$sql = "
+CREATE TABLE lschool_db.images (
+  img_name varchar(100) NOT NULL,
+  img_id int(5) NOT NULL AUTO_INCREMENT,
+  user_id int(5) NOT NULL,
+  PRIMARY KEY (img_id),
+  FOREIGN KEY (user_id) REFERENCES lschool_db.users_login (user_id)
+  ON UPDATE CASCADE
+  ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;";
 $connection->query($sql);
