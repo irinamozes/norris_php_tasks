cefa93caa30db4220688a3fef3cf7aad82a7a931
Irina Mozes <irina5687@gmail.com>
2017-04-29 23:20:59 +0500

    Туристик

--- homework9_turistik/wp-content/themes/turistik/functions.php
+++ homework9_turistik/wp-content/themes/turistik/functions.php
@@ -57,10 +57,3 @@ class BotMenuWalker extends Walker_Nav_Menu
 if (function_exists('acf_add_options_page')) {
     acf_add_options_page('Опции');
 }
-
-
-function cc_mime_types($mimes) {
-    $mimes['svg'] = 'image/svg+xml';
-    return $mimes;
-}
-add_filter('upload_mimes', 'cc_mime_types');
--- homework9_turistik/wp-content/themes/turistik/part/sidebar.php
+++ homework9_turistik/wp-content/themes/turistik/part/sidebar.php
@@ -2,34 +2,25 @@
     <div class="sidebar__sidebar-item">
         <div class="sidebar-item__title">Метки</div>
         <div class="sidebar-item__content">
-            <ul class="tags-list">
-                <?php $args = array(
-                    'smallest'                  => 0.9,
-                    'largest'                   => 0.9,
-                    'unit'                      => 'rem',
-                    'number'                    => 45,
-                    'format'                    => 'array',
-                    'separator'                 => "\n",
-                    'orderby'                   => 'name',
-                    'order'                     => 'ASC',
-                    'exclude'                   => null,
-                    'include'                   => null,
-                    'topic_count_text_callback' => '',
-                    'link'                      => 'view',
-                    'echo'                      => false,
-                    'child_of'                  => null,
-                );
-                $tags = (wp_tag_cloud($args));
-                ?>
-                <?php foreach ($tags as $tag) : ?>
-                    <?php $a = new SimpleXMLElement($tag); ?>
-                    <li class="tags-list__item">
-                        <a href="<?php echo $a['href']; ?>" class="tags-list__item__link">
-                            <?php echo (string)$a; ?>
-                        </a></li>
-                <?php endforeach; ?>
+                        <ul class="tags-list">
+            <?php
+
+            $tags = get_tags();
+            foreach ($tags as $tag) :
+            ?>
+
+                <li class="tags-list__item">
+                    <a href="<?php echo get_term_link($tag); ?>" class="tags-list__item__link"><?php echo $tag->name; ?></a>
+                </li>
+            <?php endforeach; ?>
             </ul>
         </div>
+
+        <?php
+
+//echo get_the_category_list();
+
+        ?>
     </div>
     <div class="sidebar__sidebar-item">
         <div class="sidebar-item__title">Рубрики</div>
